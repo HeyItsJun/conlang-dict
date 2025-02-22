69a344f4781d05a34c6b3fc6ded55af969a1a201
br0223 <elena.araujo@alumnos.upm.es>
2025-02-22 21:58:31 +0100

    being redundant is a lifestyle.

--- gscript.js
+++ gscript.js
@@ -2,13 +2,13 @@
 const relDataPath = "./data.json"; // defines where the file is located
 const tableId = "results"; // defines the id used to refer to the output table
 const searchBarId = "searchbar"; // defines the id used to refer to the search bar
-const fieldToSearchBy = "word"; // defines the JSON field for entry filtering
+const selectorId = "selector"; // defines de id used in the selector in glossary
 /* ---------- */
 
-let entries = null;
+let entries = [];
 fetch(relDataPath)
     .then(response => response.json())
-    .then(data => dsearch(entries = data))
+    .then(data => load(entries = data))
     .catch(error => console.error(error));
 
 function load(data) {
@@ -17,14 +17,19 @@ function load(data) {
     data.forEach(entry => table.insertRow().insertCell().innerHTML = toString(entry));
 }
 
-function dsearch() {
+function gsearch() {
+    const field = document.getElementById(selectorId).value;
     const input = document.getElementById(searchBarId).value;
-    load(input !== "" ? entries.filter(entry => (entry[fieldToSearchBy] || "")
-        .toLowerCase()
-        .includes(input.toLowerCase())) : []);
+    load(
+        input === "" ? entries
+            : entries.filter(entry => (entry[field] || "")
+                .toLowerCase()
+                .includes(input.toLowerCase())
+            )
+    );
 }
 
 /* returns a string containing the format that an entry should have */
 function toString(entry) {
-    return [entry.word, "meaning: ", entry.description, "english: ", entry.en_translation, "tags: ", entry.tags].join("<br>");
+    return [entry.word, "meaning: ", entry.description, "english: ", entry.engtl, "categ: ", entry.categ].join("<br>");
 }
